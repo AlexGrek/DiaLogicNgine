ec056a2e3f08c78a2d311c4cd72080ae73614dc9
alexgrek <alexgrek.hq@gmail.com>
2023-09-17 01:52:58 +0300

    character development

--- dialogic/src/game/Character.tsx
+++ dialogic/src/game/Character.tsx
@@ -1,3 +1,22 @@
+import Prop from "./Prop";
+import { TextList } from "./TextList";
+import Proxy from "./Proxy"
+
+export interface Trait {
+    name: string
+    inherits: string[]
+    description: string
+}
+
+export interface Role {
+    name: string
+    props: Prop[]
+}
+
 export default interface Character {
-    
+    uid: string
+    displayName: TextList
+    traits: string[]
+    props: Prop[]
+    startDialog?: Proxy
 }
---
+++ dialogic/src/game/Proxy.ts
@@ -0,0 +1,12 @@
+import { UiObjectId } from "../exec/GameState";
+
+export interface ProxyDest {
+    destination: UiObjectId
+    name?: string
+}
+
+export default interface Proxy {
+    // Proxy is a portal to other dialogs/windows/anything
+    destinations: ProxyDest[]
+    proxyRouterScript: string
+}
